d1f90f3eb72999f3001fa5322ab40e8c632116f9
LarsVerschoor <larsverschoor2@gmail.com>
2026-08-20 00:17:04 +0200

    Fixed typo that caused a bug

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ const server = http.createServer((req, res) => {
 	}
 
 	res.writeHead(200, {
-		'Content-Type": "text/plain; charset=utf-8',
+		'Content-Type': 'text/plain; charset=utf-8',
 	});
 
 	res.end(config);
