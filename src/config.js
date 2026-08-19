a848513b81f89326254e23d147d85c7661a6b1bc
LarsVerschoor <larsverschoor2@gmail.com>
2026-08-20 00:15:31 +0200

    Added HTTP server

---
+++ src/config.js
@@ -0,0 +1,22 @@
+import { createInterface } from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { encrypt } from "unixcrypt";
+
+export default async function getConfig() {
+	console.log('\nAnswer the following questions to generate the debian preseed:\n');
+
+	const rl = createInterface({ input, output });
+
+	const values = {
+		username: await rl.question('Username: '),
+		user_fullname: await rl.question('Full name: '),
+		user_password: encrypt(await rl.question(`User password: `), '$6$rounds=200000'),
+		ssh_public_key: await rl.question('SSH public key: '),
+	};
+
+	rl.close();
+
+	return values;
+}
+
+
---
+++ src/server.js
@@ -0,0 +1,35 @@
+import http from "node:http";
+import { readFile } from "node:fs/promises";
+import getLocalIpAddress from "./ip-address.js";
+import getConfig from "./config.js";
+
+const [config, template] = await Promise.all([
+	getConfig(),
+	readFile('./preconfiguration.txt', 'utf8')
+]);
+
+let preseed = template;
+
+for (const [key, value] of Object.entries(config)) {
+	preseed = preseed.replaceAll(`{{${key}}}`, value);
+}
+
+console.log('\nPreseed config made.');
+
+const server = http.createServer((req, res) => {
+	if (req.method !== 'GET' || req.url !== '/preseed') {
+		res.writeHead(404);
+		res.end('Not found');
+		return;
+	}
+
+	res.writeHead(200, {
+		'Content-Type": "text/plain; charset=utf-8',
+	});
+
+	res.end(config);
+});
+
+server.listen(3000, () => {
+	console.log(`\nHTTP server is listening, add \"http://${getLocalIpAddress()}:3000/preseed\" to your boot parameters.`);
+});
